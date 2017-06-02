bdb5b6c62305766ae5f737ef8b01e6da050e0579
Luke Childs <lukechilds123@gmail.com>
2017-06-02 14:32:46 +0700

    Test server instance exposes useful properties

--- test/create-test-server.js
+++ test/create-test-server.js
@@ -8,3 +8,13 @@ test('createTestServer is a function', t => {
 test('createTestServer returns a Promise', t => {
 	t.true(createTestServer() instanceof Promise);
 });
+
+test('server instance exposes useful properties', async t => {
+	const server = await createTestServer();
+
+	t.is(server.host, 'localhost');
+	t.true(typeof server.port === 'number');
+	t.is(server.url, `http://${server.host}:${server.port}`);
+	t.true(typeof server.listen === 'function');
+	t.true(typeof server.close === 'function');
+});
