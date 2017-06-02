71e2403aadc4dfb5111258bade0e09dae738d239
Luke Childs <lukechilds123@gmail.com>
2017-06-02 13:47:52 +0700

    Add server functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-const createTestServer = () => {
+const http = require('http');
+const express = require('express');
+const getPort = require('get-port');
+const pify = require('pify');
 
-};
+const createTestServer = () => getPort().then(port => {
+	const app = express();
+	const server = http.createServer(app);
+
+	app.host = 'localhost';
+	app.port = port;
+	app.url = `http://${app.host}:${app.port}`;
+	app.listen = pify(server.listen.bind(server, app.port));
+	app.close = pify(server.close.bind(server));
+
+	return app.listen().then(() => app);
+});
 
 module.exports = createTestServer;
