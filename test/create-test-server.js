a8e4ae4b55af7452e5c4682e0bb8255f24c9244a
Luke Childs <lukechilds123@gmail.com>
2017-06-02 14:27:08 +0700

    Test createTestServer returns a Promise

--- test/create-test-server.js
+++ test/create-test-server.js
@@ -4,3 +4,7 @@ import createTestServer from '../';
 test('createTestServer is a function', t => {
 	t.is(typeof createTestServer, 'function');
 });
+
+test('createTestServer returns a Promise', t => {
+	t.true(createTestServer() instanceof Promise);
+});
